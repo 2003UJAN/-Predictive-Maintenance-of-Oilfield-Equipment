9f108bf39925b3683b8aececa43351757bc8012f
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-11 03:13:26 +0530

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_data(filepath):
-    try:
-        df = pd.read_csv(filepath)
-        return df
-    except FileNotFoundError:
-        raise Exception(f"File not found: {filepath}")
