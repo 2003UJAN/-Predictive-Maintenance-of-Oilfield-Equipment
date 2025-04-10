a58e951463dc713ecd7d4ac0ae6572ea0cd33538
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2025-04-11 02:59:46 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def load_data(filepath):
-    return pd.read_csv(filepath)
+    try:
+        df = pd.read_csv(filepath)
+        return df
+    except FileNotFoundError:
+        raise Exception(f"File not found: {filepath}")
